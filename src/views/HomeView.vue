d0119572accba2676bf5c400ddfb64ad3255833c
fingle <fingle0618@gmail.com>
2022-09-19 18:35:10 +0800

    【add】生命周期

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <HelloWorld :msg="msg" />
+    <button @click="test">测试</button>
   </div>
 </template>
 
@@ -11,8 +11,44 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
+  data: function () {
+    return {
+      msg: "0",
+    };
+  },
+  methods: {
+    test() {
+      this.msg = this.msg + "2";
+      console.log("test");
+    },
+  },
   components: {
     HelloWorld,
+  },
+  beforeCreate: () => {
+    console.log("beforeCreate");
+  },
+  created: () => {
+    console.log("created");
+  },
+  beforeMount: () => {
+    console.log("beforeMount");
+  },
+  mounted: () => {
+    console.log("mounted");
+  },
+
+  beforeUpdate: () => {
+    console.log("beforeUpdate");
+  },
+  updated: () => {
+    console.log("updated");
+  },
+  beforeDestroy: () => {
+    console.log("beforeDestroy");
+  },
+  destroyed: () => {
+    console.log("destroyed");
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  //outputDir: "./d",
-  chainWebpack: (config) => {
-    config.entry("index").add("./src/main.js");
+  devServer: {
+    hot: true,
   },
 });
