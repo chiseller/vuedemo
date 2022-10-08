0d08404d7f2de3facbed3458462bd075cb978795
fingle <fingle0618@gmail.com>
2022-10-08 22:35:12 +0800

    【add】test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="flag = !flag">点击</button>
+    <transition>
+      <h1 v-if="flag">This is an about page</h1>
+    </transition>
   </div>
 </template>
+<style lang="stylus" scoped>
+
+h1
+  color:red
+
+.v-enter,
+.v-leave-to
+    opacity: 0;
+    color: #000000
+    transform: translateX(80px)
+
+.v-enter-to,
+.v-leave
+  color:red
+
+.v-enter-active,
+.v-leave-active
+    transition: all 12s ease
+</style>
+<script>
+// 创建 Vue 实例，得到 ViewModel
+export default {
+  data: function () {
+    return {
+      flag: false,
+    };
+  },
+  methods: {},
+};
+</script>
